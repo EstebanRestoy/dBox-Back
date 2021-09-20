1db4fdbf2eeb4d323e1cc9961fd76a1fe45ebc74
Esteban Kali <e.restoy24@gmail.com>
2021-09-20 16:52:13 +0200

    [ADD] .env + mongo driver

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json())
+
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+let db
+ 
+MongoClient.connect(url, function(err, client) {
+  console.log("Connected successfully to server");
+  db = client.db(dbName);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
